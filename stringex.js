8b0f2de769459d52e1d0dc6d6ca32084300dc496
Divya-Macha <107928158+Divya-Macha@users.noreply.github.com>
2023-02-13 10:27:29 +0530

    Add files via upload

---
+++ stringex.js
@@ -0,0 +1,15 @@
+let s = "hello";
+console.log(s);
+let r = `list of students:
+rohith
+venkat
+vaibhav`;
+let s1= 'list of students :\nrohith\nvenkat\nvaibhav';
+
+let s2 = "kmit";
+s2[2]="k";
+console.log(s2[2]);
+
+let s3 = "kmit"
+console.log(s3.substring(1,4));
+console.log(s);
